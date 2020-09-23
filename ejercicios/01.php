ac0e1de9388ba59ee0813cfa9bf9109a4e07fd55
albertoortegafernandez <alorfer87@gmail.com>
2020-09-23 15:45:24 +0100

    ejercicio1

---
+++ ejercicios/01.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio_01</title>
+</head>
+<body>
+<?php
+    $nombre= "Alberto";
+    $apellidos="Ortega Fernández";
+    $edad=32;
+    $telefono=685573811;
+    $email="alorfer@gmail.com";
+    ?>
+    <table>
+        <tr>
+            <td>Nombre: </td>
+            <td> <?php print $nombre ?></td>
+        </tr>
+        <tr>
+            <td><?php echo 'Apellidos: ' ?></td>
+            <td><?php echo $apellidos ?></td>
+        </tr>
+        <tr>
+            <td><? echo "Edad: " ?></td>
+            <td><?php print $edad ?></td>
+        </tr>
+        <tr>
+            <td>Teléfono: </td>
+            <td><?php echo $telefono ?></td>
+        </tr>
+        <tr>
+            <td>Email: </td>
+            <td><?php print $email ?></td>
+        </tr>
+    </table>
+    
+</body>
+
+</html>
---
+++ ejercicios/02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ejercicio02</title>
+    </head>
+</head>
+<body>
+    <?php
+    define('ultimo', 10);
+    $number = 8;
+
+    for ($row = 1; $row <= ultimo; $row++) {
+        
+    }
+    ?>
+</body>
+</html>
