9381628782c7118fbde88792aa6626397853433c
albertoortegafernandez <alorfer87@gmail.com>
2020-09-23 16:44:17 +0100

    ejercicio3

---
+++ ejercicios/03.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ejercicio02</title>
+    </head>
+</head>
+<body>
+    <?php
+    define('ultimo', 10);
+    $number = 3;
+
+    echo "<h1>",'Los numeros divisibles por 3 hasta el 10 son: ',"</h1>";
+    for ($row = 1; $row <= ultimo; $row++) {
+        if($row % 3==0)
+        echo $row,'. ';
+    }
+    
+    ?>
+</body>
+</html>
