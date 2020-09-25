f91c027385180e8bdd8b38c44f139479b2f91e10
albertoortegafernandez <alorfer87@gmail.com>
2020-09-25 16:16:00 +0100

    ejercicio7

---
+++ ejercicios/07.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ejercicio06</title>
+    </head>
+    <body>
+        
+        <h1>PAISES DE MI ARRAY</h1>
+        
+
+             <?php
+                $paises=["España","Portugal","Italia","Francia"];
+        
+                    foreach ($paises as $pais) {
+                    echo $pais . "<br>"; 
+                    } 
+            ?>
+    </body>
+</html>
