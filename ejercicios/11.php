78f207e0a70886e22ee8eb106a2a175f7aaabbec
albertoortegafernandez <alorfer87@gmail.com>
2020-09-28 18:16:33 +0100

    ejercicio11

---
+++ ejercicios/11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejercicio11</title>
+</head>
+<body>
+    <h2>Registro de libros</h2>
+    <form method="post" action="11action.php">
+        <label>Titulo</label><input type="text" value="" name="titulo"><br>
+        <label>Autor</label><input type="text" value="" name="autor"><br>
+        <label>Editorial</label><input type="text" value="" name="editorial"><br>
+        <label>Paginas</label><input type="text" value="" name="paginas"><br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
