5fa3280d32f5b676a55f4710f5cfc81743c21b81
albertoortegafernandez <alorfer87@gmail.com>
2020-10-05 18:00:10 +0100

    ejercicio17 mejorado incompleto

--- ejercicios/17/views/factoriales.php
+++ ejercicios/17/views/factoriales.php
@@ -3,23 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mi aplicacion web</title>
+  <title>Factoriales"</title>
 </head>
 <body>
+<h1>Los números factoriales mas proximos al millón son :</h1>
 <?php
-    require_once "App.php";
-    $app=new App;
-    $app->run();   
-?>
+  foreach($factoriales as $num){  
+    echo $num." "; 
+  }
+  ?>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
