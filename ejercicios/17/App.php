449649d3185e918eff879419a26b19cdc457716e
albertoortegafernandez <alorfer87@gmail.com>
2020-10-05 18:19:39 +0100

    ejercicio17 mejorado incompleto

--- ejercicios/17/App.php
+++ ejercicios/17/App.php
@@ -21,12 +21,22 @@ class App{
         $fibonacci=[];
         $numero1=0;
         $numero2=1;
-        //echo "<h2>",'Los números de la sucesión fibonacci menores a 1 millon son: ',"</h2>";
-        //echo $numero1,", ",$numero2,", ";
+        $suma=0;
+
         $fibonacci[]=$numero1;
         $fibonacci[]=$numero2;
         
-        for ($i = 1; $i < 100000; $i++) {
+       /* while($suma<1000000){
+          $suma=$numero1+$numero2;
+          $numero1=$numero2;
+          $fibonacci[]=$suma;
+          $numero2++;
+          echo $suma;
+        }
+        include ('views/fibonacci.php');
+    }
+*/
+        /*for ($i = 1; $i < 100000; $i++) {
             $suma=$numero1+$numero2;
             $numero1=$numero2;
             $numero2=$suma;
@@ -35,7 +45,7 @@ class App{
                 }
             }
         include ('views/fibonacci.php');
-    }
+    }*/
     public function potencias2(){
         $potencias=[];
         //echo "<h2>",'Las potencias de 2 hasta 2 elevado a 24 son: ',"</h2>";
